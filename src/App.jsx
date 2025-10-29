a6395e4b362ef4dfdb356002c50d53e0225983e5
Rheno001 <renfredreuben@gmail.com>
2025-10-29 13:59:16 +0100

    starting routes

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,8 @@ function App() {
     <>
     <h1>Hello there</h1>
     <div>My name is wisdom</div>
-<<<<<<< HEAD
     <h3>Testing</h3>
-=======
     <h2>gsdbdgdgd</h2>
->>>>>>> 88297e6a8d01ed927dcb4e28e248d92dce5b5c1f
     </>
   )
 }
