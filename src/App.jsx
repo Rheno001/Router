69e6afefb1280ee71319fb85b903ade7a063d224
Rheno001 <renfredreuben@gmail.com>
2025-10-29 14:28:53 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-
+import Navbar from '../src/components/Navbar'
+import Home from '../src/pages/Home'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-    
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+
+ 
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+        <ul className='flex'>
+            <li className='text-red-400'><Link to='/'>Testing</Link></li>
+            <li className='text-red-400'><Link to='/About'>Second</Link></li>
+        </ul>
+    </div>
   )
 }
 
