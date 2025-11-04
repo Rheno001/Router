4ac16026165e87699f2bf8ea2958de9defb04d51
ukachipraise <ukachipraise80@gmail.com>
2025-11-04 10:15:57 +0100

    Update1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import Navbar from '../src/components/Navbar'
+import Footer from './components/Footer'
 import Home from '../src/pages/Home'
 import About from '../src/pages/About'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -12,6 +14,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/About' element={<About />} />
       </Routes>
+      <Footer/>
 
     </>
   )
